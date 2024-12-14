f23ee3f237c99df79824871eec77ab108a12f59c
linux-docker-2025 <newburyservices23@gmail.com>
2024-12-14 15:39:47 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "proxmox_password" {
+  description = "Password for Proxmox API"
+  type        = string
+}
+
+variable "proxmox_host" {
+  description = "Proxmox Host"
+  type        = string
+}
